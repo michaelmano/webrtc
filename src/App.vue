70a5716d785787aed2f5d4cc10a11381d28a22ac
Michael Mano <michael.mano26@gmail.com>
2022-07-29 14:04:09 +1000

    Initial Project setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+const test = 'test';
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-green-400">
+    {{ test }}
+  </h1>
+  <HelloWorld msg="Vite + Vue" />
+</template>
