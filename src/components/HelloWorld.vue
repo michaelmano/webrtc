58b39cd445105df095b877c8276ef236b79026ef
Michael Mano <michael.mano26@gmail.com>
2022-07-29 14:57:14 +1000

    Add test files and fix config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+const count = ref<number>(0);
 
-const count = ref(0)
+const increment = () => {
+    count.value += 1;
+};
+
+defineProps<{ msg: string }>();
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -30,9 +35,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
---
+++ src/tests/HelloWorld.test.ts
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils';
+import {
+    expect, describe, it,
+} from 'vitest';
+import HelloWorld from '../components/HelloWorld.vue';
+
+describe('HelloWorld.vue', () => {
+    expect(HelloWorld).toBeTruthy();
+
+    it('should display header text', async () => {
+        const msg = 'HelloWorld';
+        const wrapper = mount(HelloWorld, {
+            props: {
+                msg,
+            },
+        });
+
+        expect(wrapper.find('h1').text()).toEqual(msg);
+    });
+
+    it('should increment the button when clicked', async () => {
+        const wrapper = mount(HelloWorld);
+        await wrapper.get('button').trigger('click');
+
+        expect(wrapper.find('button').text()).toEqual('count is 1');
+    });
+});
