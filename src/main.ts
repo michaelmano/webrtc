2ae2ae4cb982b267170159c27769ac0dfdfa6c7c
Michael Mano <michael.mano26@gmail.com>
2022-07-31 21:33:15 +1000

    update entry

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue';
+// import { createPinia } from 'pinia'; // This is a store
 import './style.css';
-import App from './App.vue';
+// import App from './App.vue';
+import VueButton from './components/CoolButton.vue';
+import VueIcon from './components/CoolIcon.vue';
 
-createApp(App).mount('#app');
+// const pinia = createPinia();
+// const app = createApp(App);
+const app = createApp({});
+app.component('VueButton', VueButton);
+app.component('VueIcon', VueIcon);
+
+app.mount('#app');
