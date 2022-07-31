32b7b7c446712859d7589cad781d4fbc98d0d0e5
Michael Mano <michael.mano26@gmail.com>
2022-07-31 21:25:12 +1000

    Setup vite config to work with dotnet

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,33 @@ const vitestConfig: VitestUserConfigInterface = {
 export default defineConfig({
     test: vitestConfig.test,
     plugins: [vue()],
+    base: '/dist/',
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    build: {
+        outDir: '../wwwroot/dist',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            input: {
+                main: './src/main.ts',
+            },
+            output: {
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name].js',
+                assetFileNames: 'css/[name].[ext]',
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
+    server: {
+        hmr: {
+            protocol: 'ws',
+        },
+    },
 });
